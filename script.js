d79f83b9adf3bd4b28bdd3103d733ebbf2a2e556
JeremyDezalos <56341917+JeremyDezalos@users.noreply.github.com>
2023-05-05 12:16:35 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,66 +1,6 @@
 //the basis of this script was found here: https://codepen.io/nearee/pen/zYYENMa
 
 
-/*d3.csv('http://localhost:8000/resources/data1/core/transfers.csv', createChart);
-//var data = d3.csv.parseRows('localhost:8000/resources/data1/core/transfers.csv');
-//console.log(data)
-//createChart(data)
-
-function createChart(data) {
-	
-	//var parseDate = d3.time.format("%Y-%m-%d").parse
-	const parseTime = d3.time.format("%Y-%m-%d %H:%M:%S").parse
-
-	var filtered_data = data.filter(d => {
-		return d.careunit != ""
-	});
-
-	filtered_data = filtered_data.sort((a,b) => {
-		return +a.subject_id - +b.subject_id
-	});
-
-	var subject_id_col = d3.map(filtered_data, d => {
-		return(d.subject_id)
-	}).keys();
-
-	const random_subject_index = Math.floor(Math.random() * subject_id_col.length);
-
-	filtered_data = filtered_data.filter(d => {
-		return d.subject_id == subject_id_col[random_subject_index]
-	});
-
-	filtered_data = d3.map(filtered_data, d => {
-		return([d.careunit, (parseTime(d.outtime) - parseTime(d.intime)) / 1000])
-	}).keys();
-
-	var acc = [["", 0]]
-	for(d in filtered_data){
-		if(acc.includes(d.careunit)){
-
-		}
-	}*/
-	//var test = (parseTime(filtered_data[0].outtime) - parseTime(filtered_data[0].intime)) / 1000 //Donne la valeur en seconde
-
-	/*var grouped_by_careunit = d3.flatRollup(
-		filtered_data,
-		x => ({
-		  intime: x.map(d => d.intime),
-		  outtime: x.map(d => d.outtime)
-		}),
-		d => d.careunit
-	  );*/
-	
-	//var grouped_by_careunit = d3.group(filtered_data, d => d.careunit)
-
-	/*filtered_data = d3.map(filtered_data, d => {
-
-	});*/
-
-	//console.log(filtered_data)
-	/*console.log(parseTime(filtered_data[0].outtime))
-	console.log(parseTime(filtered_data[0].intime))*/
-//}
-
 "use strict";
 /*[pan and well CSS scrolls]*/
 var pnls = document.querySelectorAll('.panel').length,
@@ -79,6 +19,7 @@ function _scrollY(obj) {
 	if (pan === undefined) {
 		return;
 	}
+	console.log(pan)
 	plength = plength || parseInt(pan.offsetHeight / vmin);
 	slength = parseInt(pan.style.transform.replace('translateY(', ''));
 	//compute # pixels to scroll
@@ -140,8 +81,6 @@ async function createDiv(){
 	divToCreate.innerHTML = "Lots of data for the patient !!!";
 	let func = "destroy('panelToDelete')"
 	divToCreate.innerHTML += '<br/><button onclick='+ func +'>Go back</button>';
-	
-	divToCreate.innerHTML += '<br/><img src="images/Panel3.png" class="center" alt="Example">';
 	divReference.parentNode.appendChild(divToCreate);
 	
 	//scroll down
@@ -157,7 +96,7 @@ async function createDiv(){
 async function destroy(elem){
 	canCreatePanel = true;
 	//set the destination to top
-	scdir = 'up';
+	scdir = 'top';
 	_scrollY(well);
 	let d = document.getElementById(elem);
 	const node = document.createTextNode("Go down");
@@ -169,4 +108,105 @@ async function destroy(elem){
 	
 	d.parentNode.removeChild(d);
 	
+}
+
+
+//Use as example: https://d3-graph-gallery.com/graph/barplot_horizontal.html
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 30, bottom: 40, left: 90},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#divCreator")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv('https://github.com/com-480-data-visualization/project-2023-mimic/blob/website/transfers.csv', createChart);
+
+function createChart(data) {
+
+	const parseTime = d3.time.format("%Y-%m-%d %H:%M:%S").parse
+
+	var filtered_data = data.filter(d => {
+		return d.careunit != ""
+	});
+
+	filtered_data = filtered_data.sort((a,b) => {
+		return +a.subject_id - +b.subject_id
+	});
+
+	var subject_id_col = d3.map(filtered_data, d => {
+		return(d.subject_id)
+	}).keys();
+
+	const random_subject_index = Math.floor(Math.random() * subject_id_col.length);
+
+	filtered_data = filtered_data.filter(d => {
+		return d.subject_id == subject_id_col[random_subject_index]
+	});
+
+	filtered_data = d3.map(filtered_data, d => {
+		return([d.careunit, (parseTime(d.outtime) - parseTime(d.intime)) / 1000])
+	}).keys();
+
+	var acc = 0; //Contains all time value
+	const wards = new Map();
+	for(var d in filtered_data){
+		var name = filtered_data[d].split(",")[0]
+		var time_value = parseInt(filtered_data[d].split(",")[1])
+		acc += time_value
+		if(wards.has(name)){
+			var old_value = wards.get(name)
+			time_value += old_value
+			wards.set(name, time_value)
+		}
+		else{
+			wards.set(name, time_value)
+		}
+	}
+
+	const arr = Array.from(wards, function (entry) {
+		return { key: entry[0], value: entry[1] };
+	});
+	  
+	// Add X axis
+	var x = d3.scaleLinear()
+	  .domain([0, 100])
+	  .range([0, width]);
+
+	function randomColor(){
+		var color = "#"
+		for(let j = 0; j < 6; ++j){
+			color = color + Math.floor(Math.random() * 10)
+		}
+		return color
+	}
+
+	var total_x = 0;
+	svg.selectAll("myRect")
+	.data(arr)
+	.enter()
+	.append("rect")
+	.on("click", function(d) {
+		createDiv()
+	})
+	.attr("id", function(d) {
+		return d.key
+	})
+	.attr("x", function(d) { 
+		total_x += x((d.value/acc)*100)
+		return total_x - x((d.value/acc)*100)
+	})
+	.attr("y", 100)
+	.attr("width", function(d) {
+		return x((d.value/acc)*100); })
+	.attr("height", 100)
+	.attr("fill", function(d) {
+		return randomColor()
+	})
 }
